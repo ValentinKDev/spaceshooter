5f5c5afd93862009f8c9a2056f9ac59e0c5be421
ValentinKDev <98110864+ValentinKDev@users.noreply.github.com>
2023-01-04 12:31:33 +0100

    more readable code

--- app/src/main/java/com/mobilegame/spaceshooter/logic/model/screen/inGameScreens/motions/Motions.kt
+++ app/src/main/java/com/mobilegame/spaceshooter/logic/model/screen/inGameScreens/motions/Motions.kt
@@ -3,6 +3,3 @@ package com.mobilegame.spaceshooter.logic.model.screen.inGameScreens.motions
 enum class Motions {
     UpRightNorth, UpRightSouth, UpLeftNorth, UpLeftSouth, DownRightNorth, DownRightSouth, DownLeftNorth, DownLeftSouth, None
 }
-//enum class Motions {
-//    UpRight, UpLeft, DownRight, DownLeft, None
-//}
